486c161d94d781c06edf9e1559aead9077a9239e
indamutsa <arsichizy@gmail.com>
2021-11-05 11:02:45 +0000

    We added oauth2 from google

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
 const app = require('express')()
 require('dotenv').config();
 
-
-const port = process.env.PORT || 3020
 const  nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
+const port = process.env.PORT || 3020;
 
 
-app.get('/sendmail, (req, res) =>{
-   
-    const transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: "a4d97faebd0f42",
-            pass: "0cd4380880256a"
-        }
+// SECRET ID
+const CLIENT_ID = process.env.CLIENT_ID;
+const SECRET_ID = process.env.SECRET_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+console.log(process.env.CLIENT_ID)
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  SECRET_ID,
+  REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+// Send email function
+async function sendMail() {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'jerekaarsene@gmail.com',
+        clientId: CLIENT_ID,
+        clientSecret: SECRET_ID,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
     });
-
 
     const mailOptions = {
-        from: 'ceozentrum@gmail.com',
-        to: 'jerekaarsene@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
+      from: 'SENDER NAME <jerekaarsene@gmail.com>',
+      to: 'jerekaarsene@gmail.com',
+      subject: 'Hello from gmail using API',
+      text: 'Hello from gmail email using API',
+      html: '<h1>Hello from gmail email using API</h1>',
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            res.status(500).json("Error encountered: " + error.message)
-   i     } else {
-            res.status(200).json("Email sent successfully" + info.response);
-        }
-    });
+    const result = await transport.sendMail(mailOptions);
+    return result;
+  } catch (error) {
+    return error;
+  }
+}
+
+app.get('/sendmail', (req, res) =>{
+   sendMail()
+      .then((result) => {
+          console.log('Email sent...', result)
+          res.status(200).json("Email sent... " + result)
+      })
+      .catch((error) =>{
+          console.log(error.message)      
+          res.status(500).json("Error occured, check the logs... " + error.message)
+      });
 
 })
 
